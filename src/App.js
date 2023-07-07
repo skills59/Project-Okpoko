81bc50fadec91f9155b2ad6da380f18ac5f2904d
olatunde <olatundeewuola@gmail.com>
2023-07-07 14:47:46 +0100

    a default react app for test purposes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1><b>Project Okpoko</b></h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
